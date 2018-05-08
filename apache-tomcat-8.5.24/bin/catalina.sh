14c8df074470cc5d848681618fee62be8fda0971
jjrodgom <jose.rodriguez.gomez@ricoh.es>
2018-05-08 09:35:56 +0200

    Corrección de formato de variables Linux.

--- apache-tomcat-8.5.24/bin/catalina.sh
+++ apache-tomcat-8.5.24/bin/catalina.sh
@@ -108,8 +108,9 @@
 # -----------------------------------------------------------------------------
 
 JAVA_HOME="C:\Java_64\jdk1.8.0_131"
-JAVA_OPTS="$JAVA_OPTS -Dmonitoriza.properties.path=%CATALINA_HOME%/conf/staticMonitorizaConfig.properties"
-JAVA_OPTS="$JAVA_OPTS -Dlog4j.configuration=file:/%CATALINA_HOME%/conf/monitoriza-log4j.xml -Dspring.config.location=%CATALINA_HOME%/conf/application.properties"
+JAVA_OPTS="$JAVA_OPTS -Dmonitoriza.properties.path=$CATALINA_BASE/conf/staticMonitorizaConfig.properties"
+JAVA_OPTS="$JAVA_OPTS -Dlog4j.configuration=file:$CATALINA_BASE/conf/monitoriza-log4j.xml"
+JAVA_OPTS="$JAVA_OPTS -Dfile.encoding=UTF-8"
 
 -Djava.security.policy=="$CATALINA_BASE"/conf/catalina.policy \
 
