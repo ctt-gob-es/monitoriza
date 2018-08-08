d2d19c5e9c640aa49966cdc319f41ac08b100d90
jjrodgom <jose.rodriguez.gomez@ricoh.es>
2018-08-08 08:59:15 +0200

    Resolución de conflicto debido al formateo del código

--- monitoriza-web/src/main/java/es/gob/monitoriza/spring/config/WebSecurityConfig.java
+++ monitoriza-web/src/main/java/es/gob/monitoriza/spring/config/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-/* 
+/*
 /*******************************************************************************
  * Copyright (C) 2018 MINHAFP, Gobierno de España
  * This program is licensed and may be used, modified and redistributed under the  terms
@@ -14,27 +14,30 @@
  * http:joinup.ec.europa.eu/software/page/eupl/licence-eupl
  ******************************************************************************/
 
-/** 
+/**
  * <b>File:</b><p>es.gob.monitoriza.config.WebSecurityConfig.java.</p>
  * <b>Description:</b><p>Class that enables and configures Spring Web Security for the Monitoriz@ application.</p>
-  * <b>Project:</b><p>Application for monitoring the services of @firma suite systems</p>
+ * <b>Project:</b><p>Application for monitoring the services of @firma suite systems</p>
  * <b>Date:</b><p>6 mar. 2018.</p>
  * @author Gobierno de España.
  * @version 1.0, 6 mar. 2018.
  */
 package es.gob.monitoriza.spring.config;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-import es.gob.monitoriza.service.impl.UserDetailServiceImpl;
+import es.gob.monitoriza.constant.GeneralConstants;
 
-/** 
+/**
  * <p>Class that enables and configures Spring Web Security for the Monitoriz@ application.</p>
  * <b>Project:</b><p>Application for monitoring services of @firma suite systems.</p>
  * @version 1.0, 6 mar. 2018.
@@ -42,59 +45,145 @@ import es.gob.monitoriza.service.impl.UserDetailServiceImpl;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-	
+
 	/**
-	 * Attribute that represents the injected service for user authentication. 
+	 * Attribute that represents the object that manages the log of the class.
+	 */
+	private static final Logger LOGGER = Logger.getLogger(GeneralConstants.LOGGER_NAME_MONITORIZA_LOG);
+
+	/**
+	 * Attribute that represents the injected service for user authentication.
 	 */
 	@Autowired
-    private UserDetailServiceImpl userDetailsService;
-	
+	private MultiFieldAuthenticationProvider multiFieldAuthPr;
+
+	/**
+	 * Attribute that represents the injected service for success user authentication.
+	 */
+	@Autowired
+	private SuccessHandler successHandler;
+
+	/**
+	 * Attribute that represents the injected service for failure user authentication.
+	 */
+	@Autowired
+	private FailureHandler failureHandler;
+
 	/**
 	 * {@inheritDoc}
+	 * @throws
 	 * @see org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter#configure(org.springframework.security.config.annotation.web.builders.HttpSecurity)
 	 */
 	@Override
-    protected void configure(HttpSecurity http) throws Exception {
-        
-      http
-        .authorizeRequests()
-        	.antMatchers("/css/**", "/images/**", "/js/**", "/fonts/**", "/fonts/icons/themify/**", "/fonts/fontawesome/**", "/less/**", "/invalidSession")
-        	.permitAll() // Enable css, images and js when logged out
-        	.and()
-        .authorizeRequests()
-          	.antMatchers("/", "add", "delete/{id}", "edit/{id}", "save", "users")
-            .permitAll()
-            .anyRequest()
-            .authenticated()
-            .and()
-        .formLogin()
-            .loginPage("/")
-            .defaultSuccessUrl("/inicio")
-            .permitAll()
-            .and()
-        .logout()
-            .permitAll()
-            .and()
-        .httpBasic()
-        	.and()
-        .csrf()
-        	.disable()			//Disable CSRF
-        .sessionManagement()
-        	.maximumSessions(1)
-        	.expiredUrl("/invalidSession")
-        	.and()
-        	.invalidSessionUrl("/invalidSession");
+	protected void configure(final HttpSecurity http) {
 
-    }
+		try {
+			http.authorizeRequests()
+			    .antMatchers("/css/**", "/images/**", "/js/**", "/fonts/**", "/fonts/icons/themify/**", "/fonts/fontawesome/**", "/less/**", "/invalidSession")
+			    .permitAll() // Enable css, images and js when logged out
+				.and()
+				.authorizeRequests()
+				.antMatchers("/", "add", "delete/{id}", "edit/{id}", "save", "users")
+				.permitAll()
+				.anyRequest()
+				.authenticated()
+				.and()
+				.addFilterBefore(authenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+				.logout()
+				.permitAll()
+				.and()
+				.httpBasic()
+				.and()
+				.csrf()
+				.disable() // Disable CSRF
+				.sessionManagement()
+				.maximumSessions(1)
+				.expiredUrl("/invalidSession")
+				.and()
+				.invalidSessionUrl("/invalidSession");
+		} catch (Exception e) {
+			LOGGER.error(e.getMessage());
+		}
 
-    /**
-     * Method that sets the authentication global configuration.
-     * @param auth Object that represents the Spring security builder.
-     * @throws Exception Object that represents the exception thrown in case of error.
-     */
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
-    }
-        
+	}
+
+	/**
+	 * Filter authentication.
+	 * @return MultiFieldAuthenticationFilter filter
+	 * @throws Exception
+	 */
+	@Bean
+	public MultiFieldAuthenticationFilter authenticationFilter() {
+		MultiFieldAuthenticationFilter authFilter = new MultiFieldAuthenticationFilter();
+		authFilter.setRequiresAuthenticationRequestMatcher(new AntPathRequestMatcher("/", "POST"));
+		try {
+			authFilter.setAuthenticationManager(authenticationManager());
+		} catch (Exception e) {
+			LOGGER.error(e.getMessage());
+		}
+		authFilter.setAuthenticationSuccessHandler(successHandler);
+		authFilter.setAuthenticationFailureHandler(failureHandler);
+		authFilter.setUsernameParameter("username");
+		authFilter.setPasswordParameter("password");
+		authFilter.setSigBase64Param("signatureBase64");
+		return authFilter;
+	}
+
+	/**
+	 * Method that sets the authentication global configuration.
+	 * @param auth Object that represents the Spring security builder.
+	 * @throws Exception Object that represents the exception thrown in case of error.
+	 */
+	@Override
+	public void configure(final AuthenticationManagerBuilder auth) {
+		auth.authenticationProvider(multiFieldAuthPr);
+	}
+
+	/**
+	 * Get multiFieldAuthPr.
+	 * @return multiFieldAuthPr
+	 */
+	public MultiFieldAuthenticationProvider getMultiFieldAuthPr() {
+		return multiFieldAuthPr;
+	}
+
+	/**
+	 * Set multiFieldAuthPr.
+	 * @param multiFieldAuthPrP set multiFieldAuthPr
+	 */
+	public void setMultiFieldAuthPr(final MultiFieldAuthenticationProvider multiFieldAuthPrP) {
+		this.multiFieldAuthPr = multiFieldAuthPrP;
+	}
+
+	/**
+	 * Get successHandler.
+	 * @return successHandler
+	 */
+	public SuccessHandler getSuccessHandler() {
+		return successHandler;
+	}
+
+	/**
+	 * Set successHandler.
+	 * @param successHandlerP set successHandler
+	 */
+	public void setSuccessHandler(final SuccessHandler successHandlerP) {
+		this.successHandler = successHandlerP;
+	}
+
+	/**
+	 * Get failureHandler.
+	 * @return failureHandler
+	 */
+	public FailureHandler getFailureHandler() {
+		return failureHandler;
+	}
+
+	/**
+	 * Set failureHandler.
+	 * @param failureHandlerP set failureHandler
+	 */
+	public void setFailureHandler(final FailureHandler failureHandlerP) {
+		this.failureHandler = failureHandlerP;
+	}
 }
