23bfab3fe8c4b8ff335350a99b265b98b532aecb
Manuel Revilla <manrevlob@gmail.com>
2018-11-09 14:11:36 +0100

    ActualizaciÃn proyecto Monitoriza-Clave
    * Eliminados directorios target
    * AÃ±adidos primera version de la generaciÃ³n de la peticion HTTP.

---
+++ monitoriza-clave-status-app/src/main/java/es/gob/monitoriza/invoker/http/conf/GenerationRequestClave.java
@@ -0,0 +1,6 @@
+package es.gob.monitoriza.invoker.http.conf;
+
+public interface GenerationRequestClave {
+
+	//public es.gob.monitoriza.invoker.http.conf.messages.GenerationRequestClaveRequestType registerClaveRequest(@WebParam(partName = "parameters", name = "registerClaveRequest", targetNamespace = "urn:es:gv:dgti:webscan:services:scanWorkBooking:messages:1.1.1") es.gva.webscangv.scanworkbooking.service.api.soap.messages.RegisterScanWorkBookingRequestType parameters) throws Exception;
+}
