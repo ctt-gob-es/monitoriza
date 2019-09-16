0fab4ccb6c0748b42f8bc07ecff6374aeeddd3a7
RICOH-EUROPE\Jose.Rodriguez.Gomez <jose.rodriguez.gomez@ricoh.es>
2019-09-16 12:13:25 +0200

    Actualización de librerías externas para la construcción del proyecto Monitoriz@.

--- resources/lib/InstallLibraries.bat
+++ resources/lib/InstallLibraries.bat
@@ -1,3 +1,4 @@
 @echo off
 call mvn install:install-file -Dfile=./es/gob/logconsumer/log-consumer-api/1.1/log-consumer-api-1.1.jar -DgroupId=es.gob.logconsumer -DartifactId=log-consumer-api -Dversion=1.1 -Dpackaging=jar -DpomFile=./es/gob/logconsumer/log-consumer-api/1.1/log-consumer-api-1.1.pom
 call mvn install:install-file -Dfile=./es/gob/logconsumer/log-consumer-register/1.1/log-consumer-register-1.1.jar -DgroupId=es.gob.logconsumer -DartifactId=log-consumer-register -Dversion=1.1 -Dpackaging=jar -DpomFile=./es/gob/logconsumer/log-consumer-register/1.1/log-consumer-register-1.1.pom
+call mvn install:install-file -Dfile=./es/gob/fire/fire-artifacts/2.4-SNAPSHOT/fire-artifacts-2.4-SNAPSHOT.pom -Dpackaging=pom -DpomFile=./es/gob/fire/fire-artifacts/2.4-SNAPSHOT/fire-artifacts-2.4-SNAPSHOT.pom
