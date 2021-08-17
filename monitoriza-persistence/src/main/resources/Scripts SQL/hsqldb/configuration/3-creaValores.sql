23aac4b6ae91047246ed74ab5de02862a6d5870b
RICOH-EUROPE\Jose.Rodriguez.Gomez <jose.rodriguez.gomez@ricoh.es>
2021-08-17 13:36:30 +0200

    Unificación de cambios en la estructura y datos de la base de datos.

--- monitoriza-persistence/src/main/resources/Scripts SQL/hsqldb/configuration/3-creaValores.sql
+++ monitoriza-persistence/src/main/resources/Scripts SQL/hsqldb/configuration/3-creaValores.sql
@@ -98,7 +98,7 @@ Insert into C_AUTHENTICATION_TYPE
    (ID_AUTHENTICATION_TYPE, TOKEN_NAME)
  Values
    (3, 'Certificado');
-   
+  
  -- TABLA ALARM 
 Insert into ALARM (ID_ALARM, DESCRIPTION, ACTIVE) Values ('ALM_001', 'ALARMDESC001', 'Y');
 Insert into ALARM (ID_ALARM, DESCRIPTION, ACTIVE) Values ('ALM_002', 'ALARMDESC002', 'Y');
@@ -111,11 +111,69 @@ Insert into ALARM (ID_ALARM, DESCRIPTION, ACTIVE) Values ('ALM_008', 'ALARMDESC0
 Insert into ALARM (ID_ALARM, DESCRIPTION, ACTIVE) Values ('ALM_009', 'ALARMDESC009', 'Y');
 
  -- TABLA SPIE_TYPE 
-Insert into SPIE_TYPE (ID_SPIE_TYPE, TOKENNAME, CONTEXT, ID_PLATFORM_TYPE, SEMAPHORE_LEVEL) Values (1, 'HSM Connection', '/spie/checkStatusInf?opCodes=4', 1, 1);
-Insert into SPIE_TYPE (ID_SPIE_TYPE, TOKENNAME, CONTEXT, ID_PLATFORM_TYPE, SEMAPHORE_LEVEL) Values (2, 'Emergency mode', '/spie/checkStatusInf?opCodes=2', 1, 1);
-Insert into SPIE_TYPE (ID_SPIE_TYPE, TOKENNAME, CONTEXT, ID_PLATFORM_TYPE, SEMAPHORE_LEVEL) Values (3, 'TS@ Connection', '/spie/checkStatusInf?opCodes=5', 1, 2);
-Insert into SPIE_TYPE (ID_SPIE_TYPE, TOKENNAME, CONTEXT, ID_PLATFORM_TYPE, SEMAPHORE_LEVEL) Values (4, 'Validation methods', '/spie/checkStatusInf?opCodes=6', 1, 1);
-Insert into SPIE_TYPE (ID_SPIE_TYPE, TOKENNAME, CONTEXT, ID_PLATFORM_TYPE, SEMAPHORE_LEVEL) Values (5, 'Response times', '/spie/checkStatusInf?opCodes=7', 1, 1);
-Insert into SPIE_TYPE (ID_SPIE_TYPE, TOKENNAME, CONTEXT, ID_PLATFORM_TYPE, SEMAPHORE_LEVEL) Values (6, 'HSM Connection', '/spie/checkStatusInf?opCodes=4', 2, 1);
-Insert into SPIE_TYPE (ID_SPIE_TYPE, TOKENNAME, CONTEXT, ID_PLATFORM_TYPE, SEMAPHORE_LEVEL) Values (7, 'Emergency mode', '/spie/checkStatusInf?opCodes=2', 2, 1);
-Insert into SPIE_TYPE (ID_SPIE_TYPE, TOKENNAME, CONTEXT, ID_PLATFORM_TYPE, SEMAPHORE_LEVEL) Values (8, '@Firma Connection', '/spie/checkStatusInf?opCodes=5', 2, 2);
+Insert into SPIE_TYPE (ID_SPIE_TYPE, TOKENNAME, CONTEXT, ID_PLATFORM_TYPE, SEMAPHORE_LEVEL, RESOLVER_CLASS) Values (1, 'HSM Connection', '/spie/checkStatusInf?opCodes=4', 1, 2, 'es.gob.monitoriza.spie.html.impl.HtmlHsmConnResolver');
+Insert into SPIE_TYPE (ID_SPIE_TYPE, TOKENNAME, CONTEXT, ID_PLATFORM_TYPE, SEMAPHORE_LEVEL, RESOLVER_CLASS) Values (2, 'Emergency mode', '/spie/checkStatusInf?opCodes=2', 1, 2, 'es.gob.monitoriza.spie.html.impl.HtmlEmergencyModeResolver');
+Insert into SPIE_TYPE (ID_SPIE_TYPE, TOKENNAME, CONTEXT, ID_PLATFORM_TYPE, SEMAPHORE_LEVEL, RESOLVER_CLASS) Values (3, 'TS@ Connection', '/spie/checkStatusInf?opCodes=5', 1, 3, 'es.gob.monitoriza.spie.html.impl.HtmlPlatformConnResolver');
+Insert into SPIE_TYPE (ID_SPIE_TYPE, TOKENNAME, CONTEXT, ID_PLATFORM_TYPE, SEMAPHORE_LEVEL, RESOLVER_CLASS) Values (4, 'Validation methods', '/spie/checkStatusInf?opCodes=6', 2, 2, 'es.gob.monitoriza.spie.html.impl.HtmlValMethodsResolver');
+Insert into SPIE_TYPE (ID_SPIE_TYPE, TOKENNAME, CONTEXT, ID_PLATFORM_TYPE, SEMAPHORE_LEVEL, RESOLVER_CLASS) Values (5, 'Response times', '/spie/checkStatusInf?opCodes=7', 1, 2, 'es.gob.monitoriza.spie.html.impl.HtmlAvgResponseTimeResolver');
+Insert into SPIE_TYPE (ID_SPIE_TYPE, TOKENNAME, CONTEXT, ID_PLATFORM_TYPE, SEMAPHORE_LEVEL, RESOLVER_CLASS) Values (6, 'HSM Connection', '/spie/checkStatusInf?opCodes=4', 2, 2, 'es.gob.monitoriza.spie.html.impl.HtmlHsmConnResolver');
+Insert into SPIE_TYPE (ID_SPIE_TYPE, TOKENNAME, CONTEXT, ID_PLATFORM_TYPE, SEMAPHORE_LEVEL, RESOLVER_CLASS) Values (7, 'Emergency mode', '/spie/checkStatusInf?opCodes=2', 2, 2, 'es.gob.monitoriza.spie.html.impl.HtmlEmergencyModeResolver');
+Insert into SPIE_TYPE (ID_SPIE_TYPE, TOKENNAME, CONTEXT, ID_PLATFORM_TYPE, SEMAPHORE_LEVEL, RESOLVER_CLASS) Values (8, '@Firma Connection', '/spie/checkStatusInf?opCodes=5', 2, 3, 'es.gob.monitoriza.spie.html.impl.HtmlPlatformConnResolver');
+
+-- TABLA C_NOTIFICATION_TYPE 
+   Insert into C_NOTIFICATION_TYPE
+   (ID_NOTIFICATION_TYPE, TOKEN_NAME)
+ Values
+   (1, 'NOTIFICATION_TYPE_CONFIG');
+
+   Insert into C_NOTIFICATION_TYPE
+   (ID_NOTIFICATION_TYPE, TOKEN_NAME)
+ Values
+   (2, 'NOTIFICATION_TYPE_NODE');
+   
+   Insert into C_NOTIFICATION_TYPE
+   (ID_NOTIFICATION_TYPE, TOKEN_NAME)
+ Values
+   (3, 'NOTIFICATION_TYPE_LOG');
+   
+-- TABLA C_NOTIFICATION_ORIGIN    
+   Insert into C_NOTIFICATION_ORIGIN
+   (ID_NOTIFICATION_ORIGIN, TOKEN_NAME)
+ Values
+   (1, 'CONFIG_VALIDATION_SERVICE_ORIGIN');
+   
+   Insert into C_NOTIFICATION_ORIGIN
+   (ID_NOTIFICATION_ORIGIN, TOKEN_NAME)
+ Values
+   (2, 'CONFIG_MAIL_SERVER_ORIGIN');
+   
+   Insert into C_NOTIFICATION_ORIGIN
+   (ID_NOTIFICATION_ORIGIN, TOKEN_NAME)
+ Values
+   (3, 'CONFIG_SPIE_GENERAL_ORIGIN');
+   
+   Insert into C_NOTIFICATION_ORIGIN
+   (ID_NOTIFICATION_ORIGIN, TOKEN_NAME)
+ Values
+   (4, 'REST_SERVICE_NODE_ORIGIN');
+   
+   Insert into C_NOTIFICATION_ORIGIN
+   (ID_NOTIFICATION_ORIGIN, TOKEN_NAME)
+ Values
+   (5, 'REST_SERVICE_LOG_ORIGIN');
+   
+Insert into C_NOTIFICATION_PRIORITY
+   (ID_NOTIFICATION_PRIORITY, TOKEN_NAME, LEVEL)
+ Values
+   (1, 'PRIORITY_LEVEL_IMPORTANT', 0);
+   
+   Insert into C_NOTIFICATION_PRIORITY
+   (ID_NOTIFICATION_PRIORITY, TOKEN_NAME, LEVEL)
+ Values
+   (2, 'PRIORITY_LEVEL_NORMAL', 1);
+
+-- TABLA C_NOTIFICATION_PRIORITY  
+   Insert into C_NOTIFICATION_PRIORITY
+   (ID_NOTIFICATION_PRIORITY, TOKEN_NAME, LEVEL)
+ Values
+   (3, 'PRIORITY_LEVEL_LOW', 2);
